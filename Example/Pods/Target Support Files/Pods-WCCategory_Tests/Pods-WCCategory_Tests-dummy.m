260fe7345011f69885620564296a3ad087e54f24
cheng <wangzhicheng@chelun.com>
2019-09-28 00:43:08 +0800

    setup  demo

---
+++ Example/Pods/Target Support Files/Pods-WCCategory_Tests/Pods-WCCategory_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WCCategory_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WCCategory_Tests
+@end
---
+++ Example/WCCategory/WCViewController.m
@@ -0,0 +1,40 @@
+//
+//  WCViewController.m
+//  WCCategory
+//
+//  Created by 394771176 on 09/27/2019.
+//  Copyright (c) 2019 394771176. All rights reserved.
+//
+
+#import "WCViewController.h"
+//#import "WCCategory.h"
+//#import <Pods_WCCategory_Example/WCCategory.h>
+#import <WCCategory/WCCategory.h>
+
+@interface WCViewController ()
+
+@end
+
+@implementation WCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIView *v = UIView.new;
+    v.left = 100;
+    v.top = 100;
+    v.width = 100;
+    v.height = 100;
+    v.backgroundColor = [UIColor colorWithString:@"ff2345"];
+    [self.view addSubview:v];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
