58463431d7c37113e5ba4013727fb62b593ce324
cheng <wangzhicheng@chelun.com>
2020-03-18 02:02:41 +0800

    分类扩展

--- Example/WCCategory/WCViewController.m
+++ Example/WCCategory/WCViewController.m
@@ -11,7 +11,9 @@
 //#import <Pods_WCCategory_Example/WCCategory.h>
 #import <WCCategory/WCCategory.h>
 
-@interface WCViewController ()
+@interface WCViewController () {
+    id vv;
+}
 
 @end
 
@@ -28,6 +30,22 @@
     v.height = 100;
     v.backgroundColor = [UIColor colorWithString:@"ff2345"];
     [self.view addSubview:v];
+    
+//    UICREATEView(UIView, 0, 0, 100, 100, 0, nil, self);
+    
+//    UIView *view = [UIView Create:RECT(0, 0, 0, 0) autoResizing:0 bgColor:nil addToView:self];
+    
+    UICREATEViewTo(vv, UIView, 0, 0, 0, 0, AAL, @"", self);
+    
+    UICREATEImageTo(vv, UIImageView, 0, 0, 0, 0, AAL, CCCenter, nil, self);
+    
+    UICREATELabelTo(vv, UILabel, 0, 0, 0, 0, AAL, nil, TTLeft, nil, nil, self);
+    
+    UICREATEControlTo(vv, UIControl, 0, 0, 0, 0, AAL, self, nil, self);
+    
+    UICREATEBtnImgTo(vv, UIButton, 0, 0, 0, 0, AALRTB, nil, self, nil, self);
+    
+    UICREATEBtnTxtTo(vv, UIButton, 0, 0, 0, 0, AAL, nil, nil, nil, nil, nil, self);
     
 //    NSString *str = @"";
 //    str = str.md5Hash;
